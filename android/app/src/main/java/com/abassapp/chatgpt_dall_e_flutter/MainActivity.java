ce50dca2e551289cc046b44e6b975673b1dff31d
abasslanre <kalanruse@gmail.com>
2024-07-25 12:27:35 +0100

    Initial commit to Git

---
+++ android/app/src/main/java/com/abassapp/chatgpt_dall_e_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.abassapp.chatgpt_dall_e_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/openai_services.dart
@@ -0,0 +1,123 @@
+import 'dart:convert';
+
+import 'package:chatgpt_dall_e_flutter/secrets.dart';
+import 'package:http/http.dart' as http;
+
+class OpenAIService {
+  final List<Map<String, String>> messages = [];
+
+  Future<String> isArtPromptAPI(String prompt) async {
+    print(['1']);
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAIAPIKEY',
+        },
+        body: jsonEncode({
+          "model": "gpt-3.5-turbo",
+          "messages": [
+            {
+              'role': 'user',
+              'content':
+              'Does this message want to generate/draw/create an AI picture or image or something similar or related to creating an AI picture or image? : $prompt . Simply answer with a yes or no.',
+            }
+          ],
+        }),
+      );
+      print(res.body);
+      if (res.statusCode == 200) {
+        String content =
+        jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
+
+        switch (content) {
+          case 'Yes':
+          case 'yes':
+          case 'Yes.':
+          case 'yes.':
+            final res = await dallEAPI(prompt);
+            return res;
+          default:
+            final res = await chatGPTAPI(prompt);
+            return res;
+        }
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> chatGPTAPI(String prompt) async {
+    print(['2']);
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAIAPIKEY',
+        },
+        body: jsonEncode({
+          "model": "gpt-3.5-turbo",
+          "messages": messages,
+        }),
+      );
+
+      if (res.statusCode == 200) {
+        String content =
+        jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
+
+        messages.add({
+          'role': 'assistant',
+          'content': content,
+        });
+        return content;
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> dallEAPI(String prompt) async {
+    print(['3']);
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/images/generations'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAIAPIKEY',
+        },
+        body: jsonEncode({
+          'prompt': prompt,
+          'n': 1,
+        }),
+      );
+
+      if (res.statusCode == 200) {
+        String imageUrl = jsonDecode(res.body)['data'][0]['url'];
+        imageUrl = imageUrl.trim();
+
+        messages.add({
+          'role': 'assistant',
+          'content': imageUrl,
+        });
+        return imageUrl;
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
